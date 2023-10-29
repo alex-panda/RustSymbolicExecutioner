ad41b39438c198f996042140a5709e0c5be94511
alex-panda <gamesafp@gmail.com>
2023-10-29 15:42:30 -0500

    added example functions

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,17 +1,25 @@
 use std::rc::Rc;
 
-use super::{ParseResult, Span, ZeroOrMore, ParseNode, OneOfTwo, SpanOf, Map, OneOfThree, Spanned};
+use super::{ParseResult, Span, ZeroOrMore, ParseNode, OneOfTwo, SpanOf, Map, OneOfThree, Spanned, OneOrMore, AnyOfThree, Maybe, AnyOfTwo};
 
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 pub struct Arg {
-    id: Span<usize>,
-    ty: Span<usize>,
+    pub id: Span<usize>,
+    pub ty: Span<usize>,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 pub struct FuncBody {
 
+}
+
+#[derive(PartialEq, Clone)]
+pub enum ReturnType {
+    /// Return the given type.
+    Type(Span<usize>),
+    /// Function never returns.
+    Never,
 }
 
 #[derive(PartialEq)]
@@ -32,7 +40,7 @@ use ParseResult::*;
 
 pub fn parse_file(file: &str) -> ParseResult<File, String, usize> {
     // a rule that just consumes whitespace space
-    let w = SpanOf(ZeroOrMore(OneOfTwo(..32u32, 127u32)));
+    let w = SpanOf(ZeroOrMore(OneOfTwo(..=32u32, 127u32)));
 
     // a rule to parse an ascii letter (lower case or upper case)
     let alpha = SpanOf(OneOfTwo(97..=122, 65..=90));
@@ -46,12 +54,56 @@ pub fn parse_file(file: &str) -> ParseResult<File, String, usize> {
         )
     );
 
+    let param = Map(
+        (ident.clone(), w.clone(), ':', w.clone(), ident.clone()),
+        |res| {
+            res.map_value(|(id, _, _colon, _, ty)| {
+                Arg { id, ty }
+            })
+        }
+    );
+    let params = Map(
+        OneOfThree(
+            w.clone(),
+            (param.clone(), w.clone()),
+            (param.clone(), OneOrMore((w.clone(), ',', w.clone(), param.clone()))),
+        ),
+        |res| {
+            res.map_value(|any_of_three| {
+                match any_of_three {
+                    AnyOfThree::One(_) => Vec::<Arg>::new(),
+                    AnyOfThree::Two((params, _)) => Vec::from([params]),
+                    AnyOfThree::Three((param1, other_params)) => {
+                        let mut accume = Vec::with_capacity(1 + other_params.len());
+                        accume.push(param1);
+                        for (_, _comma, _, param) in other_params {
+                            accume.push(param);
+                        }
+                        accume
+                    },
+                }
+            })
+        }
+    );
+
+    let return_type = Map(
+        ("->", w.clone(), OneOfTwo(ident.clone(), '!')),
+        |res| {
+            res.map_value(|(_arrow, _, any_of_two)| {
+                match any_of_two {
+                    AnyOfTwo::One(id) => ReturnType::Type(id),
+                    AnyOfTwo::Two(_) => ReturnType::Never,
+                }
+            })
+        }
+    );
+
     // the rule to parse a `Func`
     let func_rule = Rc::new(
         Map(
-            Spanned((w.clone(), "fn", w.clone(), ident.clone(), w.clone(), '(', w.clone(), ')', w.clone(), '{', w.clone(), '}', w.clone())),
+            Spanned(("fn", w.clone(), ident.clone(), w.clone(), '(', w.clone(), params.clone(), w.clone(), ')', w.clone(), return_type, w.clone(), '{', w.clone(), '}')),
             |vals| {
-                vals.map_value(|(span, (_, f, _, ident, _, oparen, _, cparen, _, ocbrace, _, ccbrace, _))| {
+                vals.map_value(|(span, (f, _, ident, _, oparen, _, params, _, cparen, _, ret_type, _, ocbrace, _, ccbrace))| {
                     Func {
                         span,
                         fn_: f,
@@ -67,8 +119,8 @@ pub fn parse_file(file: &str) -> ParseResult<File, String, usize> {
     // the rule to parse a `File`
     let file_rule = Rc::new(
         Map(
-            ZeroOrMore(func_rule.clone()),
-            |_: ParseResult<Vec<Func>, String, usize>| {
+            ZeroOrMore((w.clone(), func_rule.clone(), w.clone())),
+            |_: ParseResult<Vec<(Span<usize>, Func, Span<usize>)>, String, usize>| {
                 Okay(File {
                     funcs: Vec::new(),
                 })
@@ -76,7 +128,7 @@ pub fn parse_file(file: &str) -> ParseResult<File, String, usize> {
         )
     );
 
-    file_rule.parse(&file, 0)
+    file_rule.parse(&file, 0usize)
 }
 
 
