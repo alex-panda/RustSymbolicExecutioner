fc9cc444a7069e3eca7475739f514e43b9a6cb4f
CWNELSON4 <cwnelson4@wisc.edu>
2023-11-05 19:47:11 -0600

    Added base compiler and test file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 mod parser;
+mod compiler;
 
+use std::env;
 
 fn main() {
-    print!("Hello!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("File name expected");
+        return;
+    }
+    println!("{}", &args[1]);
+
+    let valid = compiler::compile_input(&args[1]);
+    if valid {
+        println!("Hello World!");
+    }
+
+    else {
+        println!("Could not compile");
+    }
 }
